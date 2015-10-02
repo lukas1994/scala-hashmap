1ac792666ba72173455f22c91536086b967edaf1
Lukas Koebis <lukaskoebis@gmail.com>
2015-10-02 13:48:59 +0200

    added Main file

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.11.7"
 
+mainClass in Compile := Some("Main")
+
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.4" //% "test"
 )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+
+object Main extends App {
+  val map = new HashMap[Int](100)
+  println("Created HashMap with 100 buckets...")
+
+  for (i <- 0 until 50) {
+    map.set(i.toString, i)
+  }
+  println("Set 50 values...")
+
+  println(s"Load factor: ${map.load}")
+
+  for (i <- 0 until 25) {
+    map.delete(i.toString)
+  }
+  println("Deleted 25 values...")
+
+  println(s"Load factor: ${map.load}")
+}
