18465c4f19d740fadeced0fd269f235219a5a5f4
Lukas Koebis <lukaskoebis@gmail.com>
2015-10-03 14:32:26 +0200

    improved tests

--- src/test/scala/HashMapTest.scala
+++ src/test/scala/HashMapTest.scala
@@ -35,43 +35,46 @@ trait HashMapBehaviors { this: FunSuite =>
       assert(Math.abs(map.load - 0.0) < EPS)
     }
   }
-
-  def sizeOneHashMap(createSizeOneHashMap: => HashMap[Int]) {
-    test("collisions") {
-      val map = createSizeOneHashMap
-      map.set("a", 1)
-      map.set("b", 2)
-      assert(map.get("a") === Some(1))
-      assert(map.get("b") === Some(2))
-      map.set("a", 2)
-      map.set("b", 1)
-      assert(map.get("a") === Some(2))
-      assert(map.get("b") === Some(1))
-    }
-  }
-
-  def sizeOneMaxSizeHashMap(createSizeOneMaxSizeHashMap: => HashMap[Int]) {
-    test("max size") {
-      val map = createSizeOneMaxSizeHashMap
-      map.set("a", 1)
-      intercept[RuntimeException] {
-        map.set("b", 2)
-      }
-    }
-  }
 }
 
 class BucketHashMapTestSuite extends FunSuite with HashMapBehaviors {
   def emptyHashMap = new BucketHashMap[Int](10)
-  def sizeOneHashMap = new BucketHashMap[Int](1)
 
   testsFor(emptyHashMap(emptyHashMap))
-  testsFor(sizeOneHashMap(sizeOneHashMap))
+
+  test("collisions") {
+    val map = new BucketHashMap[Int](1)
+    map.set("a", 1)
+    map.set("b", 2)
+    assert(map.get("a") === Some(1))
+    assert(map.get("b") === Some(2))
+    map.set("a", 2)
+    map.set("b", 1)
+    assert(map.get("a") === Some(2))
+    assert(map.get("b") === Some(1))
+  }
+
+  test("large dataset") {
+    val map = new BucketHashMap[Int](10)
+    for (i <- 0 until 1000) {
+      map.set(i.toString, i)
+    }
+    for (i <- 0 until 1000) {
+      assert(map.get(i.toString) === Some(i))
+      assert(map.delete(i.toString) === Some(i))
+    }
+  }
 }
 class OpenAddressingHashMapTestSuite extends FunSuite with HashMapBehaviors {
   def emptyHashMap = new OpenAddressingHashMap[Int](10)
-  def sizeOneHashMap = new OpenAddressingHashMap[Int](1)
 
   testsFor(emptyHashMap(emptyHashMap))
-  testsFor(sizeOneMaxSizeHashMap(sizeOneHashMap))
+
+  test("max size") {
+    val map = new OpenAddressingHashMap[Int](1)
+    map.set("a", 1)
+    intercept[RuntimeException] {
+      map.set("b", 2)
+    }
+  }
 }
